547aa76f2de7f6b34de5dddbadb78f30d5733da5
Octo <me@octonezd.me>
2021-09-23 15:42:56 +0300

    Add icon

--- build-exeinstaller.ps1
+++ build-exeinstaller.ps1
@@ -1,5 +1,6 @@
 mkdir installer
+magick -density 1024x1024 -background transparent favicon.svg -define icon:auto-resize installer\favicon.ico
 "" > .\installer\installer.ps1
 Get-Content .\chocoinstaller.psm1 >> installer\installer.ps1
 Get-Content .\exeinstaller-init.ps1 >> installer\installer.ps1
-invoke-ps2exe .\installer\installer.ps1 -requireAdmin
+invoke-ps2exe .\installer\installer.ps1 -requireAdmin -iconFile .\installer\favicon.ico
